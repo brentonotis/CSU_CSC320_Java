95d6c90ade1309c0ac7d57f7c3f2bbbec823bc26
Brenton Otis <36424273+brentonotis@users.noreply.github.com>
2018-07-26 23:58:05 -0500

    Create PrintStatements.java

---
+++ PrintStatements.java
@@ -0,0 +1,15 @@
+
+
+// Project Purpose: Develop pseudocode and a Java application that prints info. for a fictional person
+
+public class CTA1 {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("1. Bobby");
+		System.out.println("2. Marley");
+		System.out.println("3. 123 One Love Ave.");
+		System.out.println("4. Kingston, Jamaica");
+		System.out.println("5. 54321");
+	}
+}
