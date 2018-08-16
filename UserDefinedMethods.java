9cc47624be2bafc1cc998f9faad80a0d4c9f05fc
Brenton Otis <36424273+brentonotis@users.noreply.github.com>
2018-08-16 10:16:15 -0500

    Create UserDefinedMethods.java

---
+++ UserDefinedMethods.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class CTA5 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scnr = new Scanner(System.in);
+		
+		String paraA = "";
+		String paraB = "";
+		String paraC = "";
+		
+		String finalString = "";
+		
+		System.out.println("Enter first string: ");
+		paraA = scnr.nextLine();
+		
+		System.out.println("Enter second string: ");
+		paraB = scnr.nextLine();
+		
+		System.out.println("Enter third string: ");
+		paraC = scnr.nextLine();
+		
+		finalString = reversedConcat(paraA, paraB, paraC);
+		System.out.println("Reversed concatenated string is: " + finalString);	
+		
+	}
+	
+	public static String reversedConcat(String paraA, String paraB, String paraC) {
+		
+		String concatString = "";
+		String reversedString = "";
+		
+		concatString = paraA + paraB + paraC;
+		
+		for (int i = concatString.length() - 1; i >= 0; --i) {
+			reversedString += concatString.charAt(i);
+		}
+		return reversedString;
+		
+	}
+}
